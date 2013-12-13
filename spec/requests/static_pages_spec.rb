2f101284f102f02445c3a80e6777d93430f7e299
PC <fayake@hotmail.com>
2013-12-13 09:32:27 +0800

    about.html.erb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,21 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+    it "should have the content 'help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('help')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+
   #describe "GET /static_pages" do
   #  it "works! (now write some real specs)" do
   #    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
